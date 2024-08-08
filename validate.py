35c454ee49aae1b3b6529cc017175ef3522e9ba2
Steve Schmidt <steveschmidt@steves-air.mynetworksettings.com>
2024-08-07 20:05:55 -0400

    validate daily nulls

--- validate.py
+++ validate.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from logger import Logging
+import argparse
+
 
 logger = Logging()
 
 
-def check_duplicate_rows(filename='all_data.csv'):
-    data = pd.read_csv('all_data.csv')
+def check_duplicate_rows(data):
     ohlc_cols = ['open', 'high', 'low', 'adjusted_close', 'volume']
     dups = data[ohlc_cols].duplicated()
     duplicates = dups.sum()
@@ -15,6 +16,21 @@ def check_duplicate_rows(filename='all_data.csv'):
         logger.error(f'{duplicates} duplicate rows found, {duplicates/data.shape[0]*100} percent of data, please check the data.')
     return duplicates, duplicates/data.shape[0]*100
 
+def check_missing_daily_values(data):
+    cols = ['open', 'high', 'low', 'adjusted_close', 'volume', 'sma_20','sma_50', 'sma_200', 'ema_20', 'ema_50', 'ema_200', 'macd', 'rsi_14',
+       'bbands_upper_20', 'bbands_middle_20', 'bbands_lower_20']
+    missing_values = data[cols].isnull().sum().sum()
+    if missing_values == 0:
+        logger.info('No missing values found')
+    else:
+        logger.error(f'{missing_values} missing values found, {missing_values/data.shape[0]*100} percent of data, please check the data.')
+    return missing_values, missing_values/data.shape[0]*100
 
 if __name__ == '__main__':
-    check_duplicate_rows()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--csv", help="flat file to validate data", type=str, default='all_data.csv')
+    args = parser.parse_args()
+    data = pd.read_csv(args.csv)
+    check_duplicate_rows(data)
+    check_missing_daily_values(data)
+
