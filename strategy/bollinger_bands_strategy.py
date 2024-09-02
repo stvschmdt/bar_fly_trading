c2c7a39d8a4779bd58c9b68c3bf03c33c8630251
Steve Schmidt <steveschmidt@steves-air.mynetworksettings.com>
2024-09-01 21:30:07 -0400

    revised bollinger bands to be more realistic for testing

--- strategy/bollinger_bands_strategy.py
+++ strategy/bollinger_bands_strategy.py
@@ -1,15 +1,22 @@
 from datetime import datetime
 
+
 import pandas as pd
+import os
+import sys
+# print current working directory
 
 from order import Order, StockOrder, OrderOperation
+from account import Account
 from strategy.base_strategy import BaseStrategy
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 
 class BollingerBandsStrategy(BaseStrategy):
     def __init__(self, account, symbols):
         super().__init__(account, symbols)
-        self.historical_data = pd.read_csv('all_data.csv')
+        self.historical_data = pd.read_csv('api_data/all_data.csv')
         # Remove any columns where symbol is not in symbols
         self.historical_data = self.historical_data[self.historical_data['symbol'].isin(symbols)]
 
@@ -21,8 +28,12 @@ class BollingerBandsStrategy(BaseStrategy):
             row = self.historical_data[(self.historical_data['symbol'] == symbol) & (self.historical_data['date'] == date)]
             # If the adjusted_close column in historical_data for this symbol on date is less than the bbands_lower_20 column, then buy
             if row['adjusted_close'].iloc[0] < row['bbands_lower_20'].iloc[0]:
-                orders.append(StockOrder(symbol, OrderOperation.BUY, 1, float(current_prices.loc[current_prices['symbol'] == symbol, 'open'].iloc[0]), date))
+                # buy max shares account will offer
+                shares_to_buy = self.account.get_max_buyable_shares(float(current_prices.loc[current_prices['symbol'] == symbol, 'open'].iloc[0]), 1.0)
+                orders.append(StockOrder(symbol, OrderOperation.BUY, shares_to_buy, float(current_prices.loc[current_prices['symbol'] == symbol, 'open'].iloc[0]), date))
             elif row['adjusted_close'].iloc[0] > row['bbands_upper_20'].iloc[0]:
-                orders.append(StockOrder(symbol, OrderOperation.SELL, 1, float(current_prices.loc[current_prices['symbol'] == symbol, 'open'].iloc[0]), date))
+                # check to make sure we own shares first
+                if self.account.stock_positions.get(symbol, 0) > 0:
+                    orders.append(StockOrder(symbol, OrderOperation.SELL, self.account.stock_positions.get(symbol, 0), float(current_prices.loc[current_prices['symbol'] == symbol, 'open'].iloc[0]), date))
         return orders
 
